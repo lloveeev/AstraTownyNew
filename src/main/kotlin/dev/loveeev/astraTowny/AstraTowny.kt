2259f4e7fd8d71849f79b115e3384748dc8db1f7
loveeev <kirillkoros650@gmail.com>
2025-03-06 21:07:15 +0300

    Initial commit

---
+++ src/main/kotlin/dev/loveeev/astraTowny/AstraTowny.kt
@@ -0,0 +1,14 @@
+package dev.loveeev.astraTowny
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class AstraTowny : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
